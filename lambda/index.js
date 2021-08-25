a6ccf14083c7fe045a43ebf8cb84675d72df9a74
Fernando Martin <fer.martin@gmail.com>
2021-08-25 10:24:40 -0300

    usa funcion para tomar resoluciones de slots

--- lambda/index.js
+++ lambda/index.js
@@ -14,146 +14,146 @@
  * permissions and limitations under the License.
  **/
 
- const Alexa = require('ask-sdk-core');
- const util = require('./util');
- 
- /**
-  * API Handler for RecordColor API
-  *
-  * @param handlerInput
-  * @returns API response object
-  *
-  * See https://developer.amazon.com/en-US/docs/alexa/conversations/handle-api-calls.html
-  */
- const RequestVolunteerApiHandler = {
-     canHandle(handlerInput) {
-         return util.isApiRequest(handlerInput, 'APIRequestVolunteer');
-     },
-     handle(handlerInput) {
-         console.log("Api Request [APIRequestVolunteer]: ", JSON.stringify(handlerInput.requestEnvelope.request, null, 2));
-         // First get our request entity and grab the color passed in the API call
-         const args = util.getApiArguments(handlerInput);
-         const service = args.service;
-         const date = args.date;
-         // Store the favorite color in the session
-         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-         sessionAttributes.VolunteerRequest = {
-             service: service,
-             date: date,
-             status: 0
-         };
- 
-         return handlerInput.responseBuilder
-             .withApiResponse(sessionAttributes.VolunteerRequest)
-             .withShouldEndSession(false)
-             .getResponse();
-     }
- }
- 
- /**
-  * API Handler for GetFavoriteColor API
-  *
-  * @param handlerInput
-  * @returns API response object
-  *
-  * See https://developer.amazon.com/en-US/docs/alexa/conversations/handle-api-calls.html
-  */
- const GetFavoriteColorApiHandler = {
-     canHandle(handlerInput) {
-         return util.isApiRequest(handlerInput, 'GetFavoriteColor');
-     },
-     handle(handlerInput) {
-         console.log("Api Request [GetFavoriteColor]: ", JSON.stringify(handlerInput.requestEnvelope.request, null, 2));
- 
-         // Get the favorite color from the session
-         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-         if (sessionAttributes.favoriteColor) {
-             var color = sessionAttributes.favoriteColor;
-         }
- 
-         return handlerInput.responseBuilder
-             .withApiResponse({
-                 color: color
-             })
-             .withShouldEndSession(false)
-             .getResponse();
-     }
- }
- /**
-  * FallbackIntentHandler - Handle all other requests to the skill
-  *
-  * @param handlerInput
-  * @returns response
-  *
-  * See https://developer.amazon.com/en-US/docs/alexa/conversations/handle-api-calls.html
-  */
- const FallbackIntentHandler = {
-     canHandle(handlerInput) {
-         const request = handlerInput.requestEnvelope.request;
-         return request.type === 'IntentRequest';
-     },
-     handle(handlerInput) {
-         const intentName = handlerInput.requestEnvelope.request.intent.name;
-         console.log('In catch all intent handler. Intent invoked: ' + intentName);
-         const speechOutput = "Hmm, I'm not sure. I can help you get a volunteer. What would you like to do";
- 
-         return handlerInput.responseBuilder
-             .speak(speechOutput)
-             .reprompt(speechOutput)
-             .getResponse();
-     },
- };
- const SessionEndedRequestHandler = {
-     canHandle(handlerInput) {
-         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
-     },
-     handle(handlerInput) {
-         // Any cleanup logic goes here.
-         return handlerInput.responseBuilder.getResponse();
-     }
- };
- // Generic error handling to capture any syntax or routing errors. If you receive an error
- // stating the request handler chain is not found, you have not implemented a handler for
- // the intent being invoked or included it in the skill builder below.
- const ErrorHandler = {
-     canHandle() {
-         return true;
-     },
-     handle(handlerInput, error) {
-         console.log(`~~~~ Error handled: ${error.stack}`);
-         const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
- 
-         return handlerInput.responseBuilder
-             .speak(speakOutput)
-             .reprompt(speakOutput)
-             .getResponse();
-     }
- };
- // *****************************************************************************
- // These simple interceptors just log the incoming and outgoing request bodies to assist in debugging.
- 
- const LogRequestInterceptor = {
-     process(handlerInput) {
-         console.log(`REQUEST ENVELOPE = ${JSON.stringify(handlerInput.requestEnvelope)}`);
-     },
- };
- 
- const LogResponseInterceptor = {
-     process(handlerInput, response) {
-         console.log(`RESPONSE = ${JSON.stringify(response)}`);
-     },
- };
- // The SkillBuilder acts as the entry point for your skill, routing all request and response
- // payloads to the handlers above. Make sure any new handlers or interceptors you've
- // defined are included below. The order matters - they're processed top to bottom.
- exports.handler = Alexa.SkillBuilders.custom()
-     .addErrorHandlers(ErrorHandler)
-     .addRequestInterceptors(LogRequestInterceptor)
-     .addResponseInterceptors(LogResponseInterceptor)
-     .addRequestHandlers(
-         RequestVolunteerApiHandler,
-         FallbackIntentHandler,
-         SessionEndedRequestHandler
-     )
-     .withCustomUserAgent('eleven-volunteers/v1')
-     .lambda();
+const Alexa = require('ask-sdk-core');
+const util = require('./util');
+
+/**
+ * API Handler for RecordColor API
+ *
+ * @param handlerInput
+ * @returns API response object
+ *
+ * See https://developer.amazon.com/en-US/docs/alexa/conversations/handle-api-calls.html
+ */
+const RequestVolunteerApiHandler = {
+    canHandle(handlerInput) {
+        return util.isApiRequest(handlerInput, 'APIRequestVolunteer');
+    },
+    handle(handlerInput) {
+        console.log("Api Request [APIRequestVolunteer]: ", JSON.stringify(handlerInput.requestEnvelope.request, null, 2));
+
+        const slots = util.getAPISlotValues(handlerInput);
+        const service = slots["service"].id;
+        const date = slots["date"].resolved;
+
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        sessionAttributes.VolunteerRequest = {
+            service: service,
+            date: date,
+            status: 0
+        };
+
+        return handlerInput.responseBuilder
+            .withApiResponse(sessionAttributes.VolunteerRequest)
+            .withShouldEndSession(false)
+            .getResponse();
+    }
+}
+
+/**
+ * API Handler for GetFavoriteColor API
+ *
+ * @param handlerInput
+ * @returns API response object
+ *
+ * See https://developer.amazon.com/en-US/docs/alexa/conversations/handle-api-calls.html
+ */
+const GetFavoriteColorApiHandler = {
+    canHandle(handlerInput) {
+        return util.isApiRequest(handlerInput, 'GetFavoriteColor');
+    },
+    handle(handlerInput) {
+        console.log("Api Request [GetFavoriteColor]: ", JSON.stringify(handlerInput.requestEnvelope.request, null, 2));
+
+        // Get the favorite color from the session
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        if (sessionAttributes.favoriteColor) {
+            var color = sessionAttributes.favoriteColor;
+        }
+
+        return handlerInput.responseBuilder
+            .withApiResponse({
+                color: color
+            })
+            .withShouldEndSession(false)
+            .getResponse();
+    }
+}
+/**
+ * FallbackIntentHandler - Handle all other requests to the skill
+ *
+ * @param handlerInput
+ * @returns response
+ *
+ * See https://developer.amazon.com/en-US/docs/alexa/conversations/handle-api-calls.html
+ */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = handlerInput.requestEnvelope.request.intent.name;
+        console.log('In catch all intent handler. Intent invoked: ' + intentName);
+        const speechOutput = "Hmm, I'm not sure. I can help you get a volunteer. What would you like to do";
+
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .reprompt(speechOutput)
+            .getResponse();
+    },
+};
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.stack}`);
+        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+// *****************************************************************************
+// These simple interceptors just log the incoming and outgoing request bodies to assist in debugging.
+
+const LogRequestInterceptor = {
+    process(handlerInput) {
+        console.log(`REQUEST ENVELOPE = ${JSON.stringify(handlerInput.requestEnvelope)}`);
+    },
+};
+
+const LogResponseInterceptor = {
+    process(handlerInput, response) {
+        console.log(`RESPONSE = ${JSON.stringify(response)}`);
+    },
+};
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addErrorHandlers(ErrorHandler)
+    .addRequestInterceptors(LogRequestInterceptor)
+    .addResponseInterceptors(LogResponseInterceptor)
+    .addRequestHandlers(
+        RequestVolunteerApiHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler
+    )
+    .withCustomUserAgent('eleven-volunteers/v1')
+    .lambda();
