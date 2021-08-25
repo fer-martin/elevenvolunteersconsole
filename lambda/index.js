e202fbed67a64729ebffaa1972473b27e55520c7
Fernando Martin <fer.martin@gmail.com>
2021-08-24 21:25:55 -0300

    ajusto el lambda para la api de solicitar voluntario

--- lambda/index.js
+++ lambda/index.js
@@ -25,55 +25,27 @@
   *
   * See https://developer.amazon.com/en-US/docs/alexa/conversations/handle-api-calls.html
   */
- const RecordColorApiHandler = {
+ const RequestVolunteerApiHandler = {
      canHandle(handlerInput) {
-         return util.isApiRequest(handlerInput, 'RecordColor');
+         return util.isApiRequest(handlerInput, 'APIRequestVolunteer');
      },
      handle(handlerInput) {
-         console.log("Api Request [RecordColor]: ", JSON.stringify(handlerInput.requestEnvelope.request, null, 2));
+         console.log("Api Request [APIRequestVolunteer]: ", JSON.stringify(handlerInput.requestEnvelope.request, null, 2));
          // First get our request entity and grab the color passed in the API call
          const args = util.getApiArguments(handlerInput);
-         const color = args.color;
+         const service = args.service;
+         const date = args.date;
          // Store the favorite color in the session
          const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-         sessionAttributes.favoriteColor = color;
+         sessionAttributes.VolunteerRequest = {
+             service: service,
+             date: date,
+             status: 0
+         };
  
          return handlerInput.responseBuilder
-             .withApiResponse({
-                 color: color
-             })
+             .withApiResponse(VolunteerRequest)
              .withShouldEndSession(false)
-             .getResponse();
-     }
- }
- 
- const IntroToAlexaConversationsButtonEventHandler = {
-     canHandle(handlerInput) {
-         console.log(JSON.stringify(handlerInput.requestEnvelope));
-         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'Alexa.Presentation.APL.UserEvent'
-             && handlerInput.requestEnvelope.request.arguments[0] === 'SetFavoriteColor';
-     },
-     handle(handlerInput) {
-         return handlerInput.responseBuilder
-             .addDirective({
-                 type: 'Dialog.DelegateRequest',
-                 target: 'AMAZON.Conversations',
-                 period: {
-                     until: 'EXPLICIT_RETURN'
-                 },
-                 updatedRequest: {
-                     type: 'Dialog.InputRequest',
-                     input: {
-                         name: 'SpecifyFavoriteColor',
-                         slots: {
-                             'color': {
-                                 name: 'color',
-                                 value: handlerInput.requestEnvelope.request.arguments[1]
-                             }
-                         }
-                     }
-                 }
-             })
              .getResponse();
      }
  }
@@ -118,12 +90,12 @@
  const FallbackIntentHandler = {
      canHandle(handlerInput) {
          const request = handlerInput.requestEnvelope.request;
-         return request.type === 'IntentRequest' && request.intent.name !== 'GetFavoriteColorApiHandler' && request.intent.name !== 'RecordColorApiHandler';
+         return request.type === 'IntentRequest';
      },
      handle(handlerInput) {
          const intentName = handlerInput.requestEnvelope.request.intent.name;
          console.log('In catch all intent handler. Intent invoked: ' + intentName);
-         const speechOutput = "Hmm, I'm not sure. You can tell me your favorite color or ask me what your favorite color is. What would you like to do";
+         const speechOutput = "Hmm, I'm not sure. I can help you get a volunteer. What would you like to do";
  
          return handlerInput.responseBuilder
              .speak(speechOutput)
@@ -179,11 +151,9 @@
      .addRequestInterceptors(LogRequestInterceptor)
      .addResponseInterceptors(LogResponseInterceptor)
      .addRequestHandlers(
-         RecordColorApiHandler,
-         GetFavoriteColorApiHandler,
-         IntroToAlexaConversationsButtonEventHandler,
+         RequestVolunteerApiHandler,
          FallbackIntentHandler,
          SessionEndedRequestHandler
      )
-     .withCustomUserAgent('reference-skills/intro-to-alexa-conversations/v1')
+     .withCustomUserAgent('eleven-volunteers/v1')
      .lambda();
